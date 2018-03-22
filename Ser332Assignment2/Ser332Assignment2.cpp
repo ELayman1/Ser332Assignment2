4e8e556b988f51b80b770ffba397433858af31f3
JJanas <JJanas@asu.edu>
2018-03-21 19:52:15 -0700

    House (pretty much) Added

--- Ser332Assignment2/Ser332Assignment2.cpp
+++ Ser332Assignment2/Ser332Assignment2.cpp
@@ -2,7 +2,7 @@
 // Elizabeth Layman, Erik Isaacson, Jacob Janas
 
 #include "stdafx.h"
-#include <GL\glut.h>
+#include <glut.h>
 #include <math.h>
 
 
@@ -352,10 +352,465 @@ void display() {
 	glVertex3f(-100.0f, 30.0f, 100.0f);
 	glEnd();
 
+	//Right Roof Triangle
+	glBegin(GL_TRIANGLES);  // fill connected polygon
+	glColor3f(1.0f, 0.0f, 0.0f);  // red
+	glVertex3f(41.0f, 30.0f, 55.0f);  // vertices of the triangle 2
+	glVertex3f(41.0f, 30.0f, 20.0f);
+	glVertex3f(12.5f, 30.0f, 55.0f);
+	glEnd();
+	//Right Roof Square
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(1.0f, 0.0f, 0.0f);  // red
+	glVertex3f(41.0f, 30.0f, 55.0f);  // vertices of the square 1
+	glVertex3f(41.0f, 30.0f, 20.0f);
+	glVertex3f(70.0f, 30.0f, 20.0f);
+	glVertex3f(70.0f, 30.0f, 55.0f);
+	glEnd();
+	//Left Roof Triangle
+	glBegin(GL_TRIANGLES);  // fill connected polygon
+	glColor3f(1.0f, 0.0f, 0.0f);  // red
+	glVertex3f(-41.0f, 30.0f, 55.0f);  // vertices of the triangle 3
+	glVertex3f(-41.0f, 30.0f, 20.0f);
+	glVertex3f(-12.5f, 30.0f, 55.0f);
+	glEnd();
+	//Left Roof Square
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(1.0f, 0.0f, 0.0f);  // red
+	glVertex3f(-41.0f, 30.0f, 55.0f);  // vertices of the square 2
+	glVertex3f(-41.0f, 30.0f, 20.0f);
+	glVertex3f(-70.0f, 30.0f, 20.0f);
+	glVertex3f(-70.0f, 30.0f, 55.0f);
+	glEnd();
+	//Roof Lines
+	glLineWidth(1.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-70.0f, 30.0f, 25.0f);
+	glVertex3f(70.0f, 30.0f, 25.0f);
+	glEnd();
+	//Roof Lines
+	glLineWidth(1.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-70.0f, 30.0f, 30.0f);
+	glVertex3f(70.0f, 30.0f, 30.0f);
+	glEnd();
+	//Roof Lines
+	glLineWidth(1.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-70.0f, 30.0f, 35.0f);
+	glVertex3f(70.0f, 30.0f, 35.0f);
+	glEnd();
+	//Roof Lines
+	glLineWidth(1.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-70.0f, 30.0f, 40.0f);
+	glVertex3f(70.0f, 30.0f, 40.0f);
+	glEnd();
+	//Roof Lines
+	glLineWidth(1.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-70.0f, 30.0f, 45.0f);
+	glVertex3f(70.0f, 30.0f, 45.0f);
+	glEnd();
+	//Roof Lines
+	glLineWidth(1.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-70.0f, 30.0f, 50.0f);
+	glVertex3f(70.0f, 30.0f, 50.0f);
+	glEnd();
+	//Middle Roof Triangle
+	glBegin(GL_TRIANGLES);  // fill connected polygon
+	glColor3f(1.0f, 1.0f, 0.95f);  // off white
+	glVertex3f(-40.0f, 30.0f, 20.0f);  // vertices of the triangle 1
+	glVertex3f(40.0f, 30.0f, 20.0f);
+	glVertex3f(0.0f, 30.0f, 70.0f);
+	glEnd();
+
+	//--MainHouse------------------------------------------
+	//Main House Square
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(1.0f, 1.0f, 0.95f);  // off white
+	glVertex3f(-70.0f, 30.0f, -60.0f);  // vertices of the square 2
+	glVertex3f(-70.0f, 30.0f, 20.0f);
+	glVertex3f(70.0f, 30.0f, 20.0f);
+	glVertex3f(70.0f, 30.0f, -60.0f);
+	glEnd();
+	//--Windows--------------------------------------------
+	//High Window
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(0.6f, 0.77f, 0.8f);  // grey-blue
+	glVertex3f(-10.0f, 30.0f, 40.0f);  // vertices of the square 2
+	glVertex3f(-10.0f, 30.0f, 15.0f);
+	glVertex3f(10.0f, 30.0f, 15.0f);
+	glVertex3f(10.0f, 30.0f, 40.0f);
+	glEnd();
+	//Left Window
+	glBegin(GL_QUADS);  // fill connected polygon 
+	glColor3f(0.6f, 0.77f, 0.8f);  // grey-blue
+	glVertex3f(-60.0f, 30.0f, -50.0f);  // vertices of the square 2
+	glVertex3f(-60.0f, 30.0f, -10.0f);
+	glVertex3f(-20.0f, 30.0f, -10.0f);
+	glVertex3f(-20.0f, 30.0f, -50.0f);
+	glEnd();
+	//Window Reflection
+	glLineWidth(5.0);
+	glColor3f(1.0f, 1.0f, 1.0f); // white
+	glBegin(GL_LINES);
+	glVertex3f(-50.0f, 30.0f, -40.0f);
+	glVertex3f(-30.0f, 30.0f, -20.0f);
+	glEnd();
+	//Window Reflection
+	glLineWidth(5.0);
+	glColor3f(1.0f, 1.0f, 1.0f); // white
+	glBegin(GL_LINES);
+	glVertex3f(-42.5f, 30.0f, -42.5f);
+	glVertex3f(-27.5f, 30.0f, -27.5f);
+	glEnd();
+	//Window Reflection
+	glLineWidth(5.0);
+	glColor3f(1.0f, 1.0f, 1.0f); // white
+	glBegin(GL_LINES);
+	glVertex3f(-35.0f, 30.0f, -42.5f);
+	glVertex3f(-25.0f, 30.0f, -35.0f);
+	glEnd();
+	//Right Window
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(0.6f, 0.77f, 0.8f);  // grey-blue
+	glVertex3f(60.0f, 30.0f, -50.0f);  // vertices of the square 2
+	glVertex3f(60.0f, 30.0f, -10.0f);
+	glVertex3f(20.0f, 30.0f, -10.0f);
+	glVertex3f(20.0f, 30.0f, -50.0f);
+	glEnd();
+	//Window Reflection
+	glLineWidth(5.0);
+	glColor3f(1.0f, 1.0f, 1.0f); // white
+	glBegin(GL_LINES);
+	glVertex3f(50.0f, 30.0f, -40.0f);
+	glVertex3f(30.0f, 30.0f, -20.0f);
+	glEnd();
+	//Window Reflection
+	glLineWidth(5.0);
+	glColor3f(1.0f, 1.0f, 1.0f); // white
+	glBegin(GL_LINES);
+	glVertex3f(42.5f, 30.0f, -42.5f);
+	glVertex3f(27.5f, 30.0f, -27.5f);
+	glEnd();
+	//Window Reflection
+	glLineWidth(5.0);
+	glColor3f(1.0f, 1.0f, 1.0f); // white
+	glBegin(GL_LINES);
+	glVertex3f(35.0f, 30.0f, -42.5f);
+	glVertex3f(25.0f, 30.0f, -35.0f);
+	glEnd();
+
+	//--Floor----------------------------------------------
+	//Street
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(0.0f, 0.0f, 0.0f);  // black
+	glVertex3f(-100.0f, 30.0f, -100.0f);  // vertices of the square 2
+	glVertex3f(-100.0f, 30.0f, -80.0f);
+	glVertex3f(100.0f, 30.0f, -80.0f);
+	glVertex3f(100.0f, 30.0f, -100.0f);
+	glEnd();
+	//Concrete Square
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(0.6f, 0.6f, 0.6f);  // grey
+	glVertex3f(-100.0f, 30.0f, -60.0f);  // vertices of the square 2
+	glVertex3f(-100.0f, 30.0f, -80.0f);
+	glVertex3f(100.0f, 30.0f, -80.0f);
+	glVertex3f(100.0f, 30.0f, -60.0f);
+	glEnd();
+	//Back House Square
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(1.0f, 1.0f, 0.95f);  // off white
+	glVertex3f(85.0f, 30.0f, -60.0f);  // vertices of the square 2
+	glVertex3f(85.0f, 30.0f, -73.0f);
+	glVertex3f(-85.0f, 30.0f, -73.0f);
+	glVertex3f(-85.0f, 30.0f, -60.0f);
+	glEnd();
+	//Dirt Left Square
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(0.4f, 0.15f, 0.03f);  // brown
+	glVertex3f(-85.0f, 30.0f, -60.0f);  // vertices of the square 2
+	glVertex3f(-85.0f, 30.0f, -73.0f);
+	glVertex3f(-20.0f, 30.0f, -73.0f);
+	glVertex3f(-20.0f, 30.0f, -60.0f);
+	glEnd();
+	//Dirt Right Square
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(0.4f, 0.15f, 0.03f);  // brown
+	glVertex3f(85.0f, 30.0f, -60.0f);  // vertices of the square 2
+	glVertex3f(85.0f, 30.0f, -73.0f);
+	glVertex3f(20.0f, 30.0f, -73.0f);
+	glVertex3f(20.0f, 30.0f, -60.0f);
+	glEnd();
+	//Main Lower House Square
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(1.0f, 1.0f, 0.95f);  // off white
+	glVertex3f(-75.0f, 30.0f, -55.0f);  // vertices of the square 2
+	glVertex3f(-75.0f, 30.0f, -65.0f);
+	glVertex3f(75.0f, 30.0f, -65.0f);
+	glVertex3f(75.0f, 30.0f, -55.0f);
+	glEnd();
+
+	//--Door-----------------------------------------------
+	//Door
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(0.4f, 0.0f, 0.1f);  // brown
+	glVertex3f(-10.0f, 30.0f, -65.0f);  // vertices of the square 2
+	glVertex3f(-10.0f, 30.0f, -25.0f);
+	glVertex3f(10.0f, 30.0f, -25.0f);
+	glVertex3f(10.0f, 30.0f, -65.0f);
+	glEnd();
+	//Door Window
+	glBegin(GL_QUADS);  // fill connected polygon
+	glColor3f(0.6f, 0.77f, 0.8f);  // grey-blue
+	glVertex3f(-5.0f, 30.0f, -40.0f);  // vertices of the square 2
+	glVertex3f(-5.0f, 30.0f, -30.0f);
+	glVertex3f(5.0f, 30.0f, -30.0f);
+	glVertex3f(5.0f, 30.0f, -40.0f);
+	glEnd();
+	//-----------------------------------------------------
+
+	//MaroonHouseLines-------------------------------------
+	//Roof Diagonal Right
+	glLineWidth(10.0);
+	glColor3f(0.5f, 0.0f, 0.2f); // maroon
+	glBegin(GL_LINES);
+	glVertex3f(40.0f, 30.0f, 20.0f);
+	glVertex3f(0.0f, 30.0f, 70.0f);
+	glEnd();
+	//Roof Diagonal Left
+	glLineWidth(10.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-40.0f, 30.0f, 20.0f);
+	glVertex3f(0.0f, 30.0f, 70.0f);
+	glEnd();
+	//Roof Horizontal Left
+	glLineWidth(10.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-39.0f, 30.0f, 20.0f);
+	glVertex3f(-70.0f, 30.0f, 20.0f);
+	glEnd();
+	//Roof Horizontal Right
+	glLineWidth(10.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(39.0f, 30.0f, 20.0f);
+	glVertex3f(70.0f, 30.0f, 20.0f);
+	glEnd();
+	//Roof Vertical Left
+	glLineWidth(10.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-70.0f, 30.0f, 57.0f);
+	glVertex3f(-70.0f, 30.0f, 18.7f);
+	glEnd();
+	//Roof Vertical Right
+	glLineWidth(10.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(70.0f, 30.0f, 57.0f);
+	glVertex3f(70.0f, 30.0f, 18.7f);
+	glEnd();
+	//Roof Horizontal LowerLedge
+	glLineWidth(10.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-72.0f, 30.0f, 5.0f);
+	glVertex3f(72.0f, 30.0f, 5.0f);
+	glEnd();
+	//Roof Horizontal UpperLedge
+	glLineWidth(10.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-74.0f, 30.0f, 7.0f);
+	glVertex3f(74.0f, 30.0f, 7.0f);
+	glEnd();
+	//Left Window Horizontal UpperLedge 1
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-62.0f, 30.0f, -10.0f);
+	glVertex3f(-18.0f, 30.0f, -10.0f);
+	glEnd();
+	//Left Window Horizontal UpperLedge 2
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-64.0f, 30.0f, -8.0f);
+	glVertex3f(-16.0f, 30.0f, -8.0f);
+	glEnd();
+	//Left Window Vertical 1 
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-60.0f, 30.0f, -50.0f);
+	glVertex3f(-60.0f, 30.0f, -10.0f);
+	glEnd();
+	//Left Window Vertical 2
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-20.0f, 30.0f, -50.0f);
+	glVertex3f(-20.0f, 30.0f, -10.0f);
+	glEnd();
+	//Left Window Vertical 3
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-40.0f, 30.0f, -50.0f);
+	glVertex3f(-40.0f, 30.0f, -10.0f);
+	glEnd();
+	//Right Window Horizontal 
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-61.0f, 30.0f, -20.0f);
+	glVertex3f(-19.0f, 30.0f, -20.0f);
+	glEnd();
+	//Left Window Horizontal LowerLedge
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-61.0f, 30.0f, -50.0f);
+	glVertex3f(-19.0f, 30.0f, -50.0f);
+	glEnd();
+	//Right Window Horizontal UpperLedge 1
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(62.0f, 30.0f, -10.0f);
+	glVertex3f(18.0f, 30.0f, -10.0f);
+	glEnd();
+	//Right Window Horizontal UpperLedge 2
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(64.0f, 30.0f, -8.0f);
+	glVertex3f(16.0f, 30.0f, -8.0f);
+	glEnd();
+	//Right Window Vertical 1 
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(60.0f, 30.0f, -50.0f);
+	glVertex3f(60.0f, 30.0f, -10.0f);
+	glEnd();
+	//Right Window Vertical 2
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(20.0f, 30.0f, -50.0f);
+	glVertex3f(20.0f, 30.0f, -10.0f);
+	glEnd();
+	//Right Window Vertical 3
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(40.0f, 30.0f, -50.0f);
+	glVertex3f(40.0f, 30.0f, -10.0f);
+	glEnd();
+	//Right Window Horizontal 
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(61.0f, 30.0f, -20.0f);
+	glVertex3f(19.0f, 30.0f, -20.0f);
+	glEnd();
+	//Right Window Horizontal LowerLedge
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(61.0f, 30.0f, -50.0f);
+	glVertex3f(19.0f, 30.0f, -50.0f);
+	glEnd();
+	//High Window Left Trim
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-10.0f, 30.0f, 41.0f);  // vertices of the square 2
+	glVertex3f(-10.0f, 30.0f, 14.0f);
+	//High Window Middle Trim
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(0.0f, 30.0f, 40.0f);  // vertices of the square 2
+	glVertex3f(0.0f, 30.0f, 15.0f);
+	//High Window Right Trim
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(10.0f, 30.0f, 14.0f);
+	glVertex3f(10.0f, 30.0f, 41.0f);
+	//High Window Top Trim
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-10.0f, 30.0f, 40.0f);
+	glVertex3f(10.0f, 30.0f, 40.0f);
+	//High Window Middle Trim
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-10.0f, 30.0f, 30.0f);
+	glVertex3f(10.0f, 30.0f, 30.0f);
+	//High Window Bottom Trim
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-10.0f, 30.0f, 15.0f);
+	glVertex3f(10.0f, 30.0f, 15.0f);
+	glEnd();
+	//Door Window Horizontal Trim
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-5.0f, 30.0f, -35.0f);
+	glVertex3f(5.0f, 30.0f, -35.0f);
+	glEnd();
+	//Door Window Vertical Trim
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(0.0f, 30.0f, -30.0f);
+	glVertex3f(0.0f, 30.0f, -40.0f);
+	glEnd();
+	//Middle Roof Vertical
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(0.0f, 30.0f, 70.0f);
+	glVertex3f(0.0f, 30.0f, 50.0f);
+	glEnd();
+	//Middle Roof Horizontal
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-17.0f, 30.0f, 50.0f);
+	glVertex3f(17.0f, 30.0f, 50.0f);
+	glEnd();
+	//Middle Roof Horizontal
+	glLineWidth(5.0);
+	glColor3f(0.5f, 0.0f, 0.2f); //maroon
+	glBegin(GL_LINES);
+	glVertex3f(-17.0f, 30.0f, 50.0f);
+	glVertex3f(17.0f, 30.0f, 50.0f);
+	glEnd();
+
+
 	glClearColor(0.4f, 0.4f, 0.4f, 0.0f);
 
 	// Viewport 1: Right
-	glViewport(width * .75, 0, width, height);
+	glViewport(width * .25, 0, width, height);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(left, right, bottom, top);
